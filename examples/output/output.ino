a7233a657d0506fe05eab0ade3d2943cc90362c9
RCmags <memoryofatrufestival@gmail.com>
2022-10-30 19:57:21 -0500

    update readme

--- examples/output/output.ino
+++ examples/output/output.ino
@@ -5,7 +5,38 @@
 #include <basicMPU6050.h>       // Library for IMU sensor. See this link: https://github.com/RCmags/basicMPU6050
 #include <imuFilter.h>
 
-basicMPU6050<> imu;
+// Gyro settings:
+#define         LP_FILTER   3           // Low pass filter.                    Value from 0 to 6
+#define         GYRO_SENS   0           // Gyro sensitivity.                   Value from 0 to 3
+#define         ACCEL_SENS  0           // Accelerometer sensitivity.          Value from 0 to 3
+#define         ADDRESS_A0  LOW        // I2C address from state of A0 pin.   A0 -> GND : ADDRESS_A0 = LOW
+                                        //                                     A0 -> 5v  : ADDRESS_A0 = HIGH
+// Accelerometer offset:
+constexpr int   AX_OFFSET =  552;       // Use these values to calibrate the accelerometer. The sensor should output 1.0g if held level. 
+constexpr int   AY_OFFSET = -241;       // These values are unlikely to be zero.
+constexpr int   AZ_OFFSET = -3185;
+
+// Output scale: 
+constexpr float AX_SCALE = 1.00457;     // Multiplier for accelerometer outputs. Use this to calibrate the sensor. If unknown set to 1.
+constexpr float AY_SCALE = 0.99170;
+constexpr float AZ_SCALE = 0.98317;
+
+constexpr float GX_SCALE = 0.99764;     // Multiplier to gyro outputs. Use this to calibrate the sensor. If unknown set to 1.
+constexpr float GY_SCALE = 1.0;
+constexpr float GZ_SCALE = 1.01037;
+
+// Bias estimate:
+#define         GYRO_BAND   35          // Standard deviation of the gyro signal. Gyro signals within this band (relative to the mean) are suppresed.   
+#define         BIAS_COUNT  5000        // Samples of the mean of the gyro signal. Larger values provide better calibration but delay suppression response. 
+
+//-- Set the template parameters:
+
+basicMPU6050<LP_FILTER,  GYRO_SENS,  ACCEL_SENS, ADDRESS_A0,
+             AX_OFFSET,  AY_OFFSET,  AZ_OFFSET, 
+             &AX_SCALE,  &AY_SCALE,  &AZ_SCALE,
+             &GX_SCALE,  &GY_SCALE,  &GZ_SCALE,
+             GYRO_BAND,  BIAS_COUNT 
+            >imu;
    
 // =========== Settings ===========
 imuFilter fusion;
@@ -25,13 +56,13 @@ void setup() {
   imu.setup();
   imu.setBias();
 
-   #if FUSION
+  //#if FUSION
     // Set quaternion with gravity vector
-    fusion.setup( imu.ax(), imu.ay(), imu.az() );     
-  #else 
+  //  fusion.setup( imu.ax(), imu.ay(), imu.az() );     
+  //#else 
     // Just use gyro
     fusion.setup();                                   
-  #endif
+  //#endif
 }
 
 void loop() {
